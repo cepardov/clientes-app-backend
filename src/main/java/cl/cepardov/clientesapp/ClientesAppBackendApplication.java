a29e4fcf6b1ed914eef3192e939f2e63e99495a5
cepardov <wip015YkC1>
2019-01-24 16:17:14 -0300

    Se usa initalizr Spring, Se crea entidad Cliente

---
+++ src/main/java/cl/cepardov/clientesapp/ClientesAppBackendApplication.java
@@ -0,0 +1,14 @@
+package cl.cepardov.clientesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesAppBackendApplication.class, args);
+	}
+
+}
+
